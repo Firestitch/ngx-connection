aa5ba5b5fe0ddd74baf4994203d95ab4bc4d9644
Raymond Gigliotti <ray@firestitch.com>
2023-04-09 07:36:48 -0400

    added service provier

--- src/app/fs-connection.module.ts
+++ src/app/fs-connection.module.ts
@@ -1,12 +1,11 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { FsConnectionService } from './services/connection.service';
 
 
 @NgModule({
-  imports: [
-    CommonModule,
+  providers: [
+    FsConnectionService,
   ]
 })
 export class FsConnectionModule {
